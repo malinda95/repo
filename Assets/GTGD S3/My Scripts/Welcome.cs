39146578cf83cb2da64f19155b2dd767630b274f
Malinda <malindawimalasena@gmail.com>
2018-10-19 21:45:45 +0530

    This is the initialization of the project

---
+++ Assets/GTGD S3/My Scripts/Welcome.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter1 
+{
+    public class Welcome : MonoBehaviour
+    {
+        public string myMessage = "Welcome";
+
+        // Use this for initialization
+        void Start()
+        {
+            MyWelcomeMessage(); 
+        }
+
+        // Update is called once per frame
+       /* void Update()
+        {
+
+        }
+        */
+
+        void MyWelcomeMessage() 
+        {
+            Debug.Log(myMessage);
+        }
+    }
+}
+
