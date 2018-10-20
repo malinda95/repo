593fa60d4166fbcdb0f41eed88ecfc4b5d645950
Malinda <malindawimalasena@gmail.com>
2018-10-20 16:38:07 +0530

    Welcome Pannel

--- Assets/GTGD S3/My Scripts/Welcome.cs
+++ Assets/GTGD S3/My Scripts/Welcome.cs
@@ -1,29 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Chapter1 
 {
     public class Welcome : MonoBehaviour
     {
         public string myMessage = "Welcome";
+        private Text textWelcome;
 
         // Use this for initialization
         void Start()
         {
+            SetInitialReferences();
             MyWelcomeMessage(); 
         }
 
-        // Update is called once per frame
-       /* void Update()
+        void SetInitialReferences()
         {
-
+            textWelcome = GameObject.Find("TextWelcome").GetComponent<Text>();
         }
-        */
 
         void MyWelcomeMessage() 
         {
-            Debug.Log(myMessage);
+            if (textWelcome != null)
+            {
+                textWelcome.text = myMessage;
+            }
+            else
+            {
+                Debug.LogWarning("Welcome message is not assigned");
+            }
         }
     }
 }
